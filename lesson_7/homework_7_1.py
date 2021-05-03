2de22eab6b466dd279c98692e410f5492e8b28ae
Leena <leena.balakrishnan.nair@gmail.com>
2021-05-03 15:46:43 -0700

    updated the remove duplicate entries for dictionary code

--- lesson_7/homework_7_1.py
+++ lesson_7/homework_7_1.py
@@ -1,28 +1,39 @@
-# Enter a country and save it to variable your_choice_country. Write find_index_by_value function which will check
-# if the tuple countries contains a country of your choice. If your_choice_country is in the tuple, return its
-# index, otherwise return a -1 value
+# Enter a country and save it to variable your_choice_country. Write find_index_by_value
+# function which will check if the tuple countries contains a country of your choice. If your_choice_country is in the
+# tuple, return its index, otherwise return a -1 value
 
 countries = ('USA', 'Canada', 'United Kingdom', 'Mexico', 'Brazil', 'Argentina', 'Chili', 'South Africa', 'Egypt',
              'Morocco', 'India', 'China', 'Ukraine', 'Spain', 'France', 'Russia')
 your_choice_country = 'Canada'
 
 def find_index_by_value(value, tuple1):
-    pass
+    if value in tuple1:
+        return tuple1.index(value)
+    else:
+        return -1
+
+print(find_index_by_value(your_choice_country,countries))
 
 
-# Change the previous exercise. Enter a random number and save it to variable your_choice_number. Write
-# find_value_by_index function which will check if the tuple countries contains a country with this index.
-# If there is a value with this index your_choice_number in the tuple, return this value,
-# otherwise return a 'No such index' text
+# Change the previous exercise. Enter a random number and save it to variable your_choice_number.
+# Write find_value_by_index function which will check if the tuple countries contains a country
+# with this index. If there is a value with this index your_choice_number in the tuple, return
+# this value, otherwise return a 'No such index' text
 
-your_choice_number = 7
+import random
+your_choice_number = 15 # random.randint(0,50)
+print(your_choice_number)
 
 def find_value_by_index(index, tuple1):
-    pass
+    if len(tuple1) < index:
+        return tuple1[index]
+    else:
+        return 'No such index'
+print(find_value_by_index(your_choice_number, countries))
 
 
-# Enter a pair of values in variables new_team_name, new_team_city. Then write add_your_own_team function
-# to add them to nhl_hockey_teams dictionary, where the name will be the key.
+# Enter a pair of values in variables new_team_name, new_team_city. Then write add_your_own_team
+# function to add them to nhl_hockey_teams dictionary, where the name will be the key.
 
 new_team_name = "Wild Russians"
 new_team_city = "Moscow"
@@ -51,32 +62,44 @@ nhl_hockey_teams = {
 }
 
 def add_your_own_team(team_name, team_city):
-    pass
+    nhl_hockey_teams[team_name] = team_city
+
+add_your_own_team(new_team_name,new_team_city)
+print(nhl_hockey_teams)
 
 
 # Create two dictionaries in dict_1, dict_2 variables. Write a join_dicts function to concatenate the following
 # dictionaries to create a new one.
+# dictionaries are mutable in python
 
-dict_1 = None
-dict_2 = None
+dict_1 = {'MA':'Boston', 'NY':'Albany','IL':'Springfield'}
+dict_2 = {'FL':'Tallahasse','GA':'Atlanta','TX':'Austin'}
 
 def join_dicts(dict1, dict2):
-    pass
+    return dict1.update(dict2)
+
+join_dicts(dict_1,dict_2)
+print(dict_1, '\n', dict_2)
 
 
-# # Enter a random number and save it to number_1 variable. Then write create_numbers_dict function to generate
-# a dictionary that contains items with keys from 1 to number_1 and values in format "x": "x**2".
+# Enter a random number and save it to number_1 variable. Then write create_numbers_dict function
+# to generate a dictionary that contains items with keys from 1 to number_1 and values in format
+# "x": "x**2".
 
-number_1 = None
+number_1 = random.randint(0,5)
 
 def create_numbers_dict(number1):
-    pass
+    dict={}
+    for i in range(number1):
+        dict[i] = i**2
+    return dict
+print(create_numbers_dict(number_1))
 
 
 # Write sum_up_hockey_cups functions to sum all values in a dictionary dict_3.
 
 dict_3 = {
-    "Montreal Canadiens": 24,
+    "Montreal Canadiens": 14,
     "Toronto Maple Leafs": 13,
     "Detroit Red Wings": 11,
     "Boston Bruins": 6,
@@ -99,7 +122,12 @@ dict_3 = {
 }
 
 def sum_up_hockey_cups(teams_dict, result = 0):
-    pass
+    teams = teams_dict.keys()
+    for team in teams:
+        result += teams_dict[team]
+    return result
+
+print(sum_up_hockey_cups(dict_3))
 
 
 # Write remove_item_by_key function to remove a key True from dict_4 dictionary.
@@ -111,8 +139,11 @@ dict_4 = {
     42: 'answer'
 }
 
-def remove_item_by_key(dict1):
-    pass
+def remove_item_by_key(dict1, key):
+    dict1.pop(key)
+remove_item_by_key(dict_4, True)
+
+print(dict_4)
 
 
 dict_5 = {
@@ -135,7 +166,10 @@ dict_5 = {
 }
 
 def find_min_max(dict1):
-    pass
+    values = dict1.values()
+    print(values)
+    return (max(values), min(values))
+print(find_min_max(dict_5))
 
 
 # Write remove_duplicates functions to remove duplicates from dictionary dict_6.
@@ -152,6 +186,14 @@ dict_6 = {
     "Lucy": "german shepherd",
     "Bailey": "labrador"
 }
+print("before removing duplicates " + str(dict_6))
+#print("before removing duplicates items--- " + str(dict_6.items()))
 
-def remove_duplicates():
-    pass
+def remove_duplicates(dict):
+    result = {}
+    for key,value in dict.items():
+        if value not in result.values():
+            result[key] = value
+    return result
+#remove_duplicates(dict_6)
+print("after removing duplicate " + str(remove_duplicates(dict_6)))
