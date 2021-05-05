08a0143e8fcd25fb8d550348620e44e6c9334579
David <d.colonia@berkeley.edu>
2021-05-05 01:13:09 -0700

    homeworks_lesson_1.py

---
+++ lesson_1/homeworks_lesson_1.py
@@ -0,0 +1,153 @@
+a = 10
+b = 15
+# Sum up a and b. Write expression to the result_sum variable
+result_sum = a + b
+print(result_sum)
+
+
+##########################################
+
+c = 10
+d = 15
+# Find the difference between c and d. Save the expression to the result_diff variable
+result_diff = a - b
+print(result_diff)
+
+
+##########################################
+
+e = 10
+f = 15
+# Find the product of e and f. Save the expression to the result_mutl variable
+result_mult = e*g
+print(result_mult)
+##########################################
+
+
+g = 10
+h = 15
+# Divide g by h. Save the expression to the result_division variable
+result_division = g / h
+print(result_division)
+##########################################
+
+
+i = 10
+j = 15
+# Calculate the floored division result of i and j. Save the expression to the result_division_floored variable
+result_division_floored = i // j 
+print(result_division_floored)
+##########################################
+
+
+k = 10 
+l = 15
+# Calculate the remainder of k divided by l. Save expression to the result_division_remainder variable
+result_division_remainder = k % l 
+print(result_division_remainder)
+##########################################
+
+
+m = 5
+n = 2
+# Rise m to the power n. Save the expression to the result_power variable
+result_power = pow(m, n)
+print(result_power)
+##########################################
+
+# Convert an o variable to negative. Save the expression to the result_negative variable
+
+o = 100
+# Convert an o variable to negative. Save the expression to the result_negative variable
+result_negative = -o
+print(result_negative)
+
+
+
+#########################################################################1.2##############################################################
+
+
+a = 10
+# Finds the second power of variable 'a'. Saves the expression to result_1 variable
+result_1 = a ** 2
+print(result_1)
+
+
+##########################################
+b = 10
+# Converts integer variable 'b' to float. Saves the expression to result_2 variable
+result_2 = float(b)
+print(result_2)
+
+##########################################
+
+c = 5.04
+# Convert the float variable 'c' to integer. Saves the expression to result_3 variable
+result_3 = int(c)
+print(result_3)
+
+
+##########################################
+
+
+d = 5.04
+e = 2
+f = 1.2
+# Sum up variables d and e and then multiply the total by f. Convert result to an integer and save the expression to result_4 variable
+result_4 = int((d + 3) * f)
+print(result_4)
+
+
+
+
+#########################################################################1.3##############################################################
+
+import math
+from math import ceil
+from math import floor
+
+# Round a off to three decimal places.
+a = 10.04789
+result_1 = round(a,3)
+
+
+##########################################
+
+
+# Round up result_2 to greater value.
+result_2 = 5 / 2 * 6 + 1.25 - 4
+print(result_2)
+print(ceil(result_2))
+
+
+##########################################
+
+d = 5.04
+e = 2
+f = 1.2
+# Sum up variables d and e and then multiply the total by f. Convert result to an integer and save the expression to result_4 variable
+result_4 = int((d + 3) * f)
+print(result_4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
